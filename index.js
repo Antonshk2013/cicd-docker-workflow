346a5c9a571f3c9fe8f2e407e0e82368634349ab
Antonshk2013 <antonshk2013@gmail.com>
2025-08-22 10:29:42 +0200

    asd

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from Antons Workflow CI/CD\n');
+  res.end(`
+   ____        _   _                 
+  / __ \\      | | (_)                
+ | |  | |_ __ | |_ _ _ __   ___  ___ 
+ | |  | | '_ \\| __| | '_ \\ / _ \\/ __|
+ | |__| | | | | |_| | | | |  __/\\__ \\
+  \\____/|_| |_|\\__|_|_| |_|\\___||___/
+                                      
+Hello from Anton's Workflow 🚀
+CI/CD pipeline — commit #2 ✅
+\n`);
 });
 
 server.listen(port, () => {
